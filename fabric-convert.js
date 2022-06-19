1a06760c47cfbc6ec5763498c4d4386c4b7b1368
Kerry Smart <kerry.z.smart@gmail.com>
2022-06-19 13:44:39 -0400

    refactor to add separate function for calculating stitch count. add function for calculating final fabric size wtih margins (optional fabric count conversion)

--- fabric-convert.js
+++ fabric-convert.js
@@ -1,35 +1,20 @@
-//given dimensions and fabric count
-
-// count = fabric count (16, 14, etc)
-//over = over 1 or 2 sts 
-
 //return sizes in inches and cm for standard fabric counts
-//units must be "inch" or "cm"
+//units must be "inch", "cm", or "stitches"
 //givenCount must be 14, 16, 18, 28, 32, 36
-//over 2 assumed for 14, 16, 18
-//over 1 assumed for 28, 32, 36
+//over 1 assumed for 14, 16, 18
+//over 2 assumed for 28, 32, 36
 function convertCommonSizes(givenWidth, givenHeight, units, givenCount) {
     //check width, height
     if (Number.isNaN(givenWidth) || Number.isNaN(givenHeight)) throw new Error("Width and height must be numbers");
     if (givenHeight > 999 || givenWidth > 999) throw new Error("Too large: width and height must be less than 1000");
+    if (givenHeight <= 0 || givenWidth <= 0) throw new Error("Negative number: width and height must be greater than 0")
     
     //validate fabric count
     givenCount = Number(givenCount);
     const acceptedCounts = [14, 16, 18, 28, 32, 36];
     if (!acceptedCounts.includes(givenCount)) throw new Error(`Fabric count was ${givenCount}, but must be one of ${acceptedCounts}`);
 
-    //validate units and calculate stitchCount
-    let stitchCount;
-    switch (units) {
-        case "inch":
-            stitchCount = [givenCount * givenWidth, givenCount * givenHeight];  
-            break;
-            case "cm":
-            stitchCount = [givenCount * cmToInch(givenWidth), givenCount * cmToInch(givenHeight)];  
-            break;
-            default:
-                throw new Error("Unit error - units must be 'inch' or 'cm'")
-    }
+    let stitchCount = getStitchCount(givenWidth, givenHeight, units, givenCount);
 
     //calculate sizes
     let fabrics = {
@@ -48,24 +33,48 @@ function convertCommonSizes(givenWidth, givenHeight, units, givenCount) {
     return fabrics;
 }
 
-//TODO do i need a similar method that works with a stitch count instead of starter size?
-
 //tests
-console.log(`-----tests for convertCommonSizes----`);
-console.log(convertCommonSizes(4, 6, "inch", 14));
+// console.log(`-----tests for convertCommonSizes----`);
+// console.log(convertCommonSizes(4, 6, "inch", 14)); //ok
+// console.log(convertCommonSizes(4999, 6, "inch", 14)); //wrong width
+// console.log(convertCommonSizes(-4, 6, "inch", 14)); //negative width
+// console.log(convertCommonSizes(4, 6999, "inch", 14)); //wrong height
+// console.log(convertCommonSizes(4, 6, "hi", 14)); //wrong units
+// console.log(convertCommonSizes(4, 6, "inch", 15)); //wrong fabric count
 
+//add margins onto design
+function getFabricSize(givenWidth, givenHeight, units, givenCount, margin, finalCount) {
+    let sizes = convertCommonSizes(givenWidth, givenHeight, units, givenCount);
+    let count;
+    if (finalCount) count = finalCount;
+    else count = givenCount;
 
-// get a specific design size, for any weird fabric count
-function convertSpecificSize(givenWidth, givenHeight, units, givenCount, givenOver, convertToCount) {
-
-    // return [newHeight, newWidth]
+    console.log(sizes)
+    return {
+        inch: [Number((sizes[count].inch[0] + margin * 2).toFixed(2)), Number((sizes[count].inch[1] + margin * 2).toFixed(2))],
+        cm: [Number((sizes[count].cm[1] + margin * 2).toFixed(2)), Number((sizes[count].cm[1] + margin * 2).toFixed(2))]
+    }
 }
+// console.log(getFabricSize(4, 6, "inch", 14, 6, 18));
 
-
-//margin = how much fabric at the sides - 2" min recommended
-function calculateRequiredFabric(designWidth, designHeight, count, over, margin) {
-
+//measurement to stitch count
+function getStitchCount(givenWidth, givenHeight, units, givenCount) {
+    switch (units) {
+        case "inch":
+            return [givenCount * givenWidth, givenCount * givenHeight];  
+        case "cm":
+            return [givenCount * cmToInch(givenWidth), givenCount * cmToInch(givenHeight)];  
+        case "stitches":
+            return [givenWidth, givenHeight];
+        default:
+                throw new Error("Unit error - units must be 'inch' or 'cm'")
+    }
 }
+// console.log(getStitchCount(4, 6, "stitches", 14));
+
+
+//stitch count to measurement 
+
 
 //cm to inch
 function cmToInch(cm) {
@@ -79,8 +88,18 @@ function inToCm(inch) {
 
 module.exports = {
     convertCommonSizes,
-    convertSpecificSize,
-    calculateRequiredFabric,
+    getStitchCount,
+    getFabricSize,
     cmToInch,
     inToCm
 }
+
+// ========== things to maybe add later ========
+
+// get a specific design size, for any weird fabric count
+function convertSpecificSize(givenWidth, givenHeight, units, givenCount, givenOver, convertToCount) {
+
+    // return [newHeight, newWidth]
+}
+
+
